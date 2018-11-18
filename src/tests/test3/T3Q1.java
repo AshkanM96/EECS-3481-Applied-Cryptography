cf6d1ca4af318c5eda90aa8aa46d67ad0ee30b1c
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2018-11-17 22:56:44 -0500

    Create T3Q1.java

---
+++ src/tests/test3/T3Q1.java
@@ -0,0 +1,19 @@
+package tests.test3;
+
+import java.math.BigInteger;
+
+public class T3Q1 {
+	/**
+	 * No dependencies.
+	 */
+
+	public static void main(String[] args) {
+		final BigInteger n = new BigInteger("40057384521392344387295509139");
+		// final BigInteger e = new BigInteger("101");
+		final BigInteger d = new BigInteger("21416819447080840026842414141");
+		final BigInteger ct = new BigInteger("159911625443136560226876180");
+
+		final byte[] pt = ct.modPow(d, n).toByteArray();
+		System.out.println(new String(pt).trim());
+	}
+}
