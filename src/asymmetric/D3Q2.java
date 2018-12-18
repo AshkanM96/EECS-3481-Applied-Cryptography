2c891f024f0e84f1a4695ee0dc98f1bccfd735bc
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2018-12-17 20:53:25 -0500

    Add files via upload

---
+++ src/asymmetric/D3Q2.java
@@ -0,0 +1,66 @@
+package asymmetric;
+
+import java.math.BigInteger;
+
+import util.CryptoTools;
+
+/**
+ * ElGamal decryption use case.
+ * 
+ * @author Ashkan Moatamed
+ */
+public class D3Q2 {
+	/**
+	 * Dependencies: <code>
+	 * 		1. util.CryptoTools
+	 * </code>
+	 */
+
+	/**
+	 * Known public prime.
+	 */
+	private static final BigInteger P = BigInteger.valueOf(17);
+
+	/**
+	 * Known public base.
+	 */
+	private static final BigInteger G = BigInteger.valueOf(3);
+
+	/**
+	 * Known Bob's private key.
+	 */
+	private static final BigInteger b = BigInteger.valueOf(6);
+
+	/**
+	 * Known Bob's public key.
+	 */
+	private static final BigInteger B = BigInteger.valueOf(15);
+
+	/**
+	 * Known ciphertext pair first element.
+	 */
+	private static final BigInteger R = BigInteger.valueOf(7);
+
+	/**
+	 * Known ciphertext pair second element.
+	 */
+	private static final BigInteger T = BigInteger.valueOf(6);
+
+	/**
+	 * Prevent instantiation.
+	 */
+	private D3Q2() {
+		// Empty by design.
+	}
+
+	public static void main(String[] args) {
+		assert (D3Q2.B.equals(D3Q2.G.modPow(D3Q2.b, D3Q2.P)));
+
+		// m = t * r ^ (-b) (mod p)
+		// m = t * (1 / r (mod p)) ^ b (mod p)
+		final BigInteger rModInverse = D3Q2.R.modInverse(D3Q2.P);
+		final BigInteger m = D3Q2.T.multiply(rModInverse.modPow(D3Q2.b, D3Q2.P)).mod(D3Q2.P);
+		CryptoTools.printlnASCII(new String(m.toByteArray()), System.out);
+		System.out.println(m.toString() + "\n");
+	}
+}
