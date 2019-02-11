fbad8221474f7d8cff01d4d5ea429d645ccc109b
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2019-02-11 00:25:47 -0500

    Add files via upload

--- src/tests/test3/T3Q3.java
+++ src/tests/test3/T3Q3.java
@@ -2,28 +2,20 @@ package tests.test3;
 
 import java.math.BigInteger;
 
+import util.BigIntegerUtil;
+
 public class T3Q3 {
 	/**
-	 * No dependencies.
+	 * Dependencies: <code>
+	 * 		1. util.BigIntegerUtil
+	 * </code>
 	 */
 
 	public static void main(String[] args) {
-		final BigInteger n1 = new BigInteger("1055827021987");
-		final BigInteger n2 = new BigInteger("973491987203");
-		assert n1.gcd(n2).equals(BigInteger.ONE);
-
-		final BigInteger y1 = new BigInteger("247775631513");
-		final BigInteger y2 = new BigInteger("43368118785");
-
-		final BigInteger n1Inv = n1.modInverse(n2);
-		final BigInteger n2Inv = n2.modInverse(n1);
-
-		final BigInteger n1n2 = n1.multiply(n2);
-
-		final BigInteger lhs = y1.multiply(n2).multiply(n2Inv);
-		final BigInteger rhs = y2.multiply(n1).multiply(n1Inv);
-
-		final BigInteger x = (lhs.add(rhs)).mod(n1n2);
-		System.out.println(x);
+		final BigInteger m1 = new BigInteger("1055827021987");
+		final BigInteger m2 = new BigInteger("973491987203");
+		final BigInteger n1 = new BigInteger("247775631513");
+		final BigInteger n2 = new BigInteger("43368118785");
+		System.out.println(BigIntegerUtil.crt(n1, m1, n2, m2, false, false)[0]);
 	}
 }
