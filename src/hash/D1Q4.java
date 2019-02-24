4c9a66829ffe2a9e93bede7fceea20996efa3b9a
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2019-02-23 21:36:55 -0500

    Add files via upload

--- src/hash/D1Q4.java
+++ src/hash/D1Q4.java
@@ -1,5 +1,7 @@
 package hash;
 
+import util.StatUtil;
+
 /**
  * Generalized birthday problem use case.
  * 
@@ -7,7 +9,9 @@ package hash;
  */
 public class D1Q4 {
 	/**
-	 * No dependencies.
+	 * Dependencies: <code>
+	 * 		1. util.StatUtil
+	 * </code>
 	 */
 
 	/**
@@ -15,44 +19,6 @@ public class D1Q4 {
 	 */
 	private D1Q4() {
 		// Empty by design.
-	}
-
-	/**
-	 * @param d
-	 *            the number of days in a year
-	 * 
-	 * @param n
-	 *            the number of people
-	 * 
-	 * @return The probability of at least two people having the same birthday.
-	 * 
-	 * @throws IllegalArgumentException
-	 *             If <code>(d <= 0) || (n < 0)</code>
-	 */
-	public static double atLeastOneCollision(int d, int n) throws IllegalArgumentException {
-		if ((d <= 0) || (n < 0)) {
-			throw new IllegalArgumentException();
-		}
-
-		// Handle the simple special cases.
-		if (n <= 1) {
-			// Probability of at least one collision with 0 or 1 people is just 0.
-			return 0;
-		} else if (n >= d) {
-			/*
-			 * Probability of at least one collision with d or more people is just 1 by the Pigeon Hole
-			 * Principle.
-			 */
-			return 1;
-		}
-
-		// First compute the probability of no collisions and then subtract that from 100%.
-		double probNoCollisions = 1;
-		for (int i = 0; i != n; ++i) {
-			// probNoCollisions *= ((double) (d - i)) / d;
-			probNoCollisions *= 1 - ((double) i) / d;
-		}
-		return ((1 - probNoCollisions) * 100);
 	}
 
 	public static void main(String[] args) {
@@ -66,6 +32,6 @@ public class D1Q4 {
 		 * divisible by the modulus (i.e., 10,000) then the hash values are also uniformly distributed since
 		 * the original values (i.e., IDs) were uniformly distributed.
 		 */
-		System.out.println(D1Q4.atLeastOneCollision(10000, 100));
+		System.out.println(StatUtil.birthday1Linear(10000, 100));
 	}
 }
