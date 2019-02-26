b972e2a6d13bf633fc72fac3c777f511907154f6
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2019-02-26 10:56:34 -0500

    Add files via upload

--- src/tests/test3/T3Q3.java
+++ src/tests/test3/T3Q3.java
@@ -2,12 +2,12 @@ package tests.test3;
 
 import java.math.BigInteger;
 
-import util.BigIntegerUtil;
+import util.BigIntUtil;
 
 public class T3Q3 {
 	/**
 	 * Dependencies: <code>
-	 * 		1. util.BigIntegerUtil
+	 * 		1. util.BigIntUtil
 	 * </code>
 	 */
 
@@ -16,6 +16,6 @@ public class T3Q3 {
 		final BigInteger m2 = new BigInteger("973491987203");
 		final BigInteger n1 = new BigInteger("247775631513");
 		final BigInteger n2 = new BigInteger("43368118785");
-		System.out.println(BigIntegerUtil.crt(n1, m1, n2, m2, false, false)[0]);
+		System.out.println(BigIntUtil.crt(n1, m1, n2, m2, false, false)[0]);
 	}
 }
