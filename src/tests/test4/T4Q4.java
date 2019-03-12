2a173465221475399aee4e2e445e8aed10732abd
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2019-03-11 20:53:08 -0400

    Add files via upload

--- src/tests/test4/T4Q4.java
+++ src/tests/test4/T4Q4.java
@@ -9,25 +9,25 @@ public class T4Q4 {
 
 	public static void main(String[] args) {
 		final BigInteger p = new BigInteger("565209496520876299");
-		final BigInteger p_minus_1 = p.subtract(BigInteger.ONE);
+		final BigInteger g = BigInteger.valueOf(101L);
 		final BigInteger f = BigInteger.valueOf(13L);
+
+		final BigInteger p_minus_1 = p.subtract(BigInteger.ONE);
 		final BigInteger[] qr = p_minus_1.divideAndRemainder(f);
 		assert (qr[1].signum() == 0);
-
 		final BigInteger quotient = qr[0];
 		System.out.println("(p - 1) / " + f + " == " + quotient);
 
-		final BigInteger g = BigInteger.valueOf(101);
-
 		final BigInteger A = new BigInteger("10938180900183");
 		final BigInteger expect = A.modPow(quotient, p);
-		System.out.println("Method 2: " + expect);
+		System.out.println("expect == " + expect);
 
 		final BigInteger base = g.modPow(quotient, p);
+		System.out.println("base == g^quotient (mod p) == " + base + "\n");
 		for (BigInteger a_0 = BigInteger.ZERO, tmp = null; !a_0.equals(f); a_0 = a_0.add(BigInteger.ONE)) {
 			tmp = base.modPow(a_0, p);
 			if (tmp.equals(expect)) {
-				System.out.println("a_0 == " + a_0);
+				System.out.printf("a_0 == %2s: base^a_0 (mod p) == expect\n", a_0.toString());
 			}
 		}
 	}
--- src/tests/test4/York232.java
+++ src/tests/test4/York232.java
@@ -3,24 +3,23 @@ package tests.test4;
 import java.security.MessageDigest;
 
 public class York232 {
-	private final byte[] msg;
-	private final byte[] sha;
+	private final byte[] msg, sha;
 
 	public York232(byte[] msg) throws Exception {
 		if ((this.msg = msg) == null) {
 			throw new NullPointerException();
 		}
-		this.sha = MessageDigest.getInstance("Sha-224").digest(this.msg);
+		this.sha = MessageDigest.getInstance("SHA-224").digest(this.msg);
 	}
 
 	public byte[] getHash() {
 		final byte[] result = new byte[29];
 		if (this.msg.length == 28) {
-			// Map 28 byte messages to 0 || msg.
+			// Map messages which are 28 bytes to 0 || msg.
 			result[0] = 0;
 			System.arraycopy(this.msg, 0, result, 1, 28);
 		} else {
-			// Map non-28 byte messages to 1 || SHA-224(msg).
+			// Map messages which aren't 28 bytes to 1 || SHA-224(msg).
 			result[0] = 1;
 			System.arraycopy(this.sha, 0, result, 1, 28);
 		}
