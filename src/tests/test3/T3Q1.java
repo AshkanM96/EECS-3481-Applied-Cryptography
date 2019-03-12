809e4ae71051a6f326a6b779ebc37427e5714683
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2019-03-11 20:52:31 -0400

    Add files via upload

--- src/tests/test3/T3Q1.java
+++ src/tests/test3/T3Q1.java
@@ -9,7 +9,7 @@ public class T3Q1 {
 
 	public static void main(String[] args) {
 		final BigInteger n = new BigInteger("40057384521392344387295509139");
-		// final BigInteger e = new BigInteger("101");
+		// final BigInteger e = BigInteger.valueOf(101L);
 		final BigInteger d = new BigInteger("21416819447080840026842414141");
 		final BigInteger ct = new BigInteger("159911625443136560226876180");
 
--- src/tests/test3/T3Q3.java
+++ src/tests/test3/T3Q3.java
@@ -16,6 +16,6 @@ public class T3Q3 {
 		final BigInteger m2 = new BigInteger("973491987203");
 		final BigInteger n1 = new BigInteger("247775631513");
 		final BigInteger n2 = new BigInteger("43368118785");
-		System.out.println(BigIntUtil.crt(n1, m1, n2, m2, false, false)[0]);
+		System.out.println(BigIntUtil.crt(n1, m1, n2, m2, true, false)[0]);
 	}
 }
