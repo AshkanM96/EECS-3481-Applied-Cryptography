a1ad89da786242d8008c9d5ac6192c852e48491a
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2019-03-16 20:56:27 -0400

    Add files via upload

--- src/tests/test3/T3Q4.java
+++ src/tests/test3/T3Q4.java
@@ -2,21 +2,21 @@ package tests.test3;
 
 import java.math.BigInteger;
 
-import util.MillerRabin;
+import util.MillerRabinTopDown;
 
 public class T3Q4 {
 	/**
 	 * Dependencies: <code>
-	 * 		1. util.MillerRabin
+	 * 		1. util.MillerRabinTopDown
 	 * </code>
 	 */
 
 	public static void main(String[] args) {
 		final BigInteger n = new BigInteger("1033931178476059651954862004553");
-		final BigInteger b = MillerRabin.TWO;
+		final BigInteger b = MillerRabinTopDown.TWO;
 		System.out.println("n == " + n + "\n");
 
-		final MillerRabin m = new MillerRabin(n);
+		final MillerRabinTopDown m = new MillerRabinTopDown(n);
 		m.test(b, true);
 	}
 }
